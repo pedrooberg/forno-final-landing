c6332306fe940ccb728db4a9110d98b0e95fe2ff
pedrooberg <93162903+pedrooberg@users.noreply.github.com>
2025-05-05 03:17:27 -0300

    Update calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,10 +52,7 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" {...props} />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />,
-      }}
+      
       {...props}
     />
   );
